ddd8cfb29600a72dbf0fc21f08ec397a4f11a0dd
Madhavan Sathiamoorthi <msathiamoorthi@mimecast.com>
2024-05-04 11:26:08 +0200

    comment all mysql code

--- api/src/config/lang.service.ts
+++ api/src/config/lang.service.ts
@@ -1,89 +1,89 @@
-import {OkPacketParams, PoolConnection, ResultSetHeader} from "mysql2";
-import { Lang } from "../model/lang";
-import db from './database';
+// import {OkPacketParams, PoolConnection, ResultSetHeader} from "mysql2";
+// import { Lang } from "../model/lang";
+// import db from './database';
 
-const selectAll = (limit: string): Promise<Lang[]> => {
-    return new Promise((resolve, reject) => {
-        db.getConnection((err, connection: PoolConnection) => {
-            connection.query('SELECT * FROM nlendb limit ' + limit, (err, rs: Lang[]) => {
-                connection.release();
-                if(err) {
-                    console.log(err);
-                    return reject(err);
-                }
-                return resolve(rs);
-            });
-        });
-    });
-}
+// const selectAll = (limit: string): Promise<Lang[]> => {
+//     return new Promise((resolve, reject) => {
+//         db.getConnection((err, connection: PoolConnection) => {
+//             connection.query('SELECT * FROM nlendb limit ' + limit, (err, rs: Lang[]) => {
+//                 connection.release();
+//                 if(err) {
+//                     console.log(err);
+//                     return reject(err);
+//                 }
+//                 return resolve(rs);
+//             });
+//         });
+//     });
+// }
 
-const insert = (data: Lang) => {
-    console.log(data)
-    return new Promise((resolve, reject) => {
-        db.getConnection((err, connection) => {
-            connection.query<ResultSetHeader>('INSERT INTO nlendb SET ?', [data], (err, rs) => {
-                if(err) {
-                    console.log(err);
-                    return reject(err);
-                }
-                return resolve(data.dutch);
-            })
-        })
-    })
-}
+// const insert = (data: Lang) => {
+//     console.log(data)
+//     return new Promise((resolve, reject) => {
+//         db.getConnection((err, connection) => {
+//             connection.query<ResultSetHeader>('INSERT INTO nlendb SET ?', [data], (err, rs) => {
+//                 if(err) {
+//                     console.log(err);
+//                     return reject(err);
+//                 }
+//                 return resolve(data.dutch);
+//             })
+//         })
+//     })
+// }
 
-const find = (nl: string) => {
-    return new Promise((resolve, reject) => {
-        db.getConnection((err, connection) => {
-            connection.query('SELECT * FROM nlendb WHERE dutch = ?', [nl], (err, rs) => {
-                if(err) {
-                    console.log(err);
-                    return reject(err);
-                }
-                return resolve(rs?.[0].insertId);
-            });
-        });
-    });
-}
+// const find = (nl: string) => {
+//     return new Promise((resolve, reject) => {
+//         db.getConnection((err, connection) => {
+//             connection.query('SELECT * FROM nlendb WHERE dutch = ?', [nl], (err, rs) => {
+//                 if(err) {
+//                     console.log(err);
+//                     return reject(err);
+//                 }
+//                 return resolve(rs?.[0].insertId);
+//             });
+//         });
+//     });
+// }
 
-const del = (nl: string) => {
-    return new Promise((resolve, reject) => {
-        db.getConnection((err, connection) => {
-            connection.query('DELETE FROM nlendb WHERE dutch = ?', nl, (err, rs) => {
-                if(err) {
-                    return reject(err);
-                }
-                return resolve(rs?.[0]);
-            });
-        })
-    })
-}
+// const del = (nl: string) => {
+//     return new Promise((resolve, reject) => {
+//         db.getConnection((err, connection) => {
+//             connection.query('DELETE FROM nlendb WHERE dutch = ?', nl, (err, rs) => {
+//                 if(err) {
+//                     return reject(err);
+//                 }
+//                 return resolve(rs?.[0]);
+//             });
+//         })
+//     })
+// }
 
-const update = (data: Lang, param: string) => {
-    console.log(data.dutch, ' =====')
-    return new Promise((resolve, reject) => {
-        db.getConnection((err, connection) => {
-            connection.query('UPDATE nlendb SET ? WHERE dutch = ?', [data, param], (err, rs) => {
-                if (err) {
-                    console.log(err);
-                    return reject(err);
-                }
-                console.log('success--')
-                return resolve(rs);
-            });
-        });
-    });
-}
+// const update = (data: Lang, param: string) => {
+//     console.log(data.dutch, ' =====')
+//     return new Promise((resolve, reject) => {
+//         db.getConnection((err, connection) => {
+//             connection.query('UPDATE nlendb SET ? WHERE dutch = ?', [data, param], (err, rs) => {
+//                 if (err) {
+//                     console.log(err);
+//                     return reject(err);
+//                 }
+//                 console.log('success--')
+//                 return resolve(rs);
+//             });
+//         });
+//     });
+// }
 
-const getTotalRecord = () => {
-    return new Promise((resolve, reject) => {
-        db.getConnection((err, connection) => {
-            connection.query('SELECT COUNT(*) as total FROM nlendb', (err, rs) => {
-                // console.log(rs[0].total, ' «««');
-                return resolve(rs[0].total);
-            })
-        })
-    })
-}
+// const getTotalRecord = () => {
+//     return new Promise((resolve, reject) => {
+//         db.getConnection((err, connection) => {
+//             connection.query('SELECT COUNT(*) as total FROM nlendb', (err, rs) => {
+//                 // console.log(rs[0].total, ' «««');
+//                 return resolve(rs[0].total);
+//             })
+//         })
+//     })
+// }
 
-export default { selectAll, insert, find, del, update, getTotalRecord };
+// export default { selectAll, insert, find, del, update, getTotalRecord };
--- api/src/controllers/lang.controller.ts
+++ api/src/controllers/lang.controller.ts
@@ -1,103 +1,101 @@
-import e, { Request, Response } from 'express';
-import langService from '../config/lang.service';
+// import e, { Request, Response } from 'express';
+// import langService from '../config/lang.service';
 
-const search = (req: Request, res: Response) => {
-    console.log(req.query.nl);
-    let val: string = req.query.nl != undefined? req.query.nl.toString(): '';
-    langService.find(val)
-        .then((word) => {
-            console.log(word);
-            res.status(200).send({
-                result: word
-            });
-        }).catch(err => {
-            res.status(500).send({
-                message: 'DATABASE ERROR',
-                error: err.code
-            });
-        });
-};
+// const search = (req: Request, res: Response) => {
+//     console.log(req.query.nl);
+//     let val: string = req.query.nl != undefined? req.query.nl.toString(): '';
+//     langService.find(val)
+//         .then((word) => {
+//             console.log(word);
+//             res.status(200).send({
+//                 result: word
+//             });
+//         }).catch(err => {
+//             res.status(500).send({
+//                 message: 'DATABASE ERROR',
+//                 error: err.code
+//             });
+//         });
+// };
 
+// const getAll = (req: Request, res: Response) => {
+//     const pageQuery = req.query.page as string;
+//     const limitQuery = req.query.limit as string;
 
-const getAll = (req: Request, res: Response) => {
-    const pageQuery = req.query.page as string;
-    const limitQuery = req.query.limit as string;
+//     console.log(pageQuery, ' - ', limitQuery);
+//     let pageInt = parseInt(pageQuery, 10) || 1;
+//     let limitInt = parseInt(limitQuery, 10) || 5;
+//     const startIndex = (pageInt - 1) * limitInt;
+//     // Here we compute the LIMIT parameter for MySQL query
+//     let limit = startIndex + ',' + limitInt;
 
-    console.log(pageQuery, ' - ', limitQuery);
-    let pageInt = parseInt(pageQuery, 10) || 1;
-    let limitInt = parseInt(limitQuery, 10) || 5;
-    const startIndex = (pageInt - 1) * limitInt;
-    // Here we compute the LIMIT parameter for MySQL query
-    let limit = startIndex + ',' + limitInt;
+//     langService.selectAll(limit)
+//         .then(words => {
+//             langService.getTotalRecord().then(count => {
+//                 let pagination = {
+//                     next: (parseInt(pageQuery) === 0)? 1: (pageInt + 1),
+//                     prev: pageInt - 1,
+//                     limit: limitInt,
+//                     total: count
+//                 }
+//                 let meta = {pagination};
 
+//                 res.status(200).send({
+//                     result: words,
+//                     meta: meta// words.slice(startIndex, endIndex)
+//                 });
+//             })
+//         }).catch(err => {
+//             res.status(500).send({
+//                 message: 'DATABASE ERROR',
+//                 error: err.code
+//             });
+//         });
+//     };
 
-    langService.selectAll(limit)
-        .then(words => {
-            langService.getTotalRecord().then(count => {
-                let pagination = {
-                    next: (parseInt(pageQuery) === 0)? 1: (pageInt + 1),
-                    prev: pageInt - 1,
-                    limit: limitInt,
-                    total: count
-                }
-                let meta = {pagination};
+// const save = (req: Request, res: Response) => {
+//     console.log(req.body.dutch, '    ===============');
+//     langService.insert(req.body)
+//         .then((word) => {
+//             console.log(word);
+//             res.status(200).send({
+//                 result: word
+//             });
+//         }).catch(err => {
+//             res.status(500).send({
+//                 message: 'DATABASE ERROR',
+//                 error: err.code
+//             });
+//         });
+// };
 
-                res.status(200).send({
-                    result: words,
-                    meta: meta// words.slice(startIndex, endIndex)
-                });
-            })
-        }).catch(err => {
-            res.status(500).send({
-                message: 'DATABASE ERROR',
-                error: err.code
-            });
-        });
-    };
- async function val() { return langService.getTotalRecord(); }
-const save = (req: Request, res: Response) => {
-    console.log(req.body.dutch, '    ===============');
-    langService.insert(req.body)
-        .then((word) => {
-            console.log(word);
-            res.status(200).send({
-                result: word
-            });
-        }).catch(err => {
-            res.status(500).send({
-                message: 'DATABASE ERROR',
-                error: err.code
-            });
-        });
-};
+// const delWord = (req: Request, res: Response) => {
+//     console.log(req.params.nl);
+//     langService.del(req.params.nl)
+//         .then((nl) => {
+//             res.status(200).send({
+//                 result: `${nl} deleted successfully`
+//             })
+//         }).catch(err => {
+//                 res.status(204).send({
+//                     message: 'DELETE FAILED',
+//                     error: err.code
+//                 });
+//             });
+//         };
 
-const delWord = (req: Request, res: Response) => {
-    console.log(req.params.nl);
-    langService.del(req.params.nl)
-        .then((nl) => {
-            res.status(200).send({
-                result: `${nl} deleted successfully`
-            })
-        }).catch(err => {
-                res.status(204).send({
-                    message: 'DELETE FAILED',
-                    error: err.code
-                });
-            });
-        };
-
-const updateWord = (req: Request, res: Response) => {
-    langService.update(req.body, req.params.nl)
-        .then((word) => {
-            console.log(word);
-            res.status(200).send({
-                result: word
-            });
-        }).catch(err => {
-            res.status(500).send({
-                message: 'UPDATE ERROR',
-                error: err.code
-            });
-        });
-}
-export default { search, getAll, save, delWord, updateWord };
+// const updateWord = (req: Request, res: Response) => {
+//     langService.update(req.body, req.params.nl)
+//         .then((word) => {
+//             console.log(word);
+//             res.status(200).send({
+//                 result: word
+//             });
+//         }).catch(err => {
+//             res.status(500).send({
+//                 message: 'UPDATE ERROR',
+//                 error: err.code
+//             });
+//         });
+// }
+// export default { search, getAll, save, delWord, updateWord };
